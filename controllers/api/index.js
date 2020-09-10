4922ea7df0a8259584d9b084ae00d2cfc4fcd688
Jacob Daughtry <akutozo@gmail.com>
2020-09-10 17:01:36 -0500

    Post v1 and post routes to verify model worked

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 // const userRoutes = require('./user-routes.js');
-// const postRoutes = require('./post-routes.js');
+const postRoutes = require('./post-routes.js');
 // const commentRoutes = require('./comment-routes.js');
 // const { route } = require('./user-routes.js');
 
 // router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
+router.use('/posts', postRoutes);
 // router.use('/comments', commentRoutes);
 
 module.exports = router;
