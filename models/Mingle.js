cbdc9fc85ba6cc55b34c81c2b98577785d91e55f
Jacob Daughtry <akutozo@gmail.com>
2020-09-10 16:49:05 -0500

    Mingle v1

--- models/Mingle.js
+++ models/Mingle.js
@@ -5,8 +5,28 @@ const sequelize = require('../config/connection');
 
 class Mingle extends Model {}
 
-
-
-
+Mingle.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'mingle'
+    }
+);
 
 module.exports = Mingle;
