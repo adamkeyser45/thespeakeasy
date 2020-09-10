0ce21bb32948a2063142d7bf0f20eaba7b701f74
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-09-09 20:52:03 -0500

    made sure the connection to the server works

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const homeRoutes = require('./home-routes');
+const apiRoutes = require('./api');
+// const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+// router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router; 
