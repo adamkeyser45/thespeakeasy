beae7456c32cbccef4568c3724ad3584a4e3810c
Jacob Daughtry <akutozo@gmail.com>
2020-09-12 15:33:00 -0500

    Login and Logout

---
+++ public/javascript/logout.js
@@ -0,0 +1,17 @@
+async function logout() {
+    console.log("Logout was called")
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  
+  document.querySelector('#logout').addEventListener('click', logout);
+  
