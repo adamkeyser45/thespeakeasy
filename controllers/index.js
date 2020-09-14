fde045dcff313ceaa46bcafee05cfe45bbb5e553
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-09-14 17:49:00 -0500

    dashboard page and some styling

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-
+const dashboardRoutes = require('./dashboard-routes.js');
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 // const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 // router.use('/dashboard', dashboardRoutes);
 
