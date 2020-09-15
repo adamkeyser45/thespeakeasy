05d5e0816fa963f4b832364ba6e72979e2435c3e
Jacob Daughtry <akutozo@gmail.com>
2020-09-14 19:46:17 -0500

    Adding a My Account Page for user info

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const dashboardRoutes = require('./dashboard-routes.js');
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
-// const dashboardRoutes = require('./dashboard-routes.js');
+const myaccountRoutes = require('./myaccount-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/myaccount', myaccountRoutes);
 router.use('/api', apiRoutes);
-// router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
