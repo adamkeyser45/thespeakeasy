4c126895f6d679ce60097b72cb51f548e3574ecc
Rachel Fritz <rachelfritz0@gmail.com>
2020-09-16 11:54:59 -0500

    fixing recipe blog post

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const sess = {
 app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(routes);
 
