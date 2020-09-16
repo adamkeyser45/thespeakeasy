09357d60a6b88cddf72ab0c26349131fbd058ae8
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-09-15 21:33:23 -0500

    cleaned up search-button.js code

--- public/javascript/search-button.js
+++ public/javascript/search-button.js
@@ -1,8 +1,6 @@
 async function findDrink(event) {
     event.preventDefault();
 
-    console.log("Hello, the button works!");
-
     const drinkName = document.getElementById('drinkName').value;
     const drinkIngr = document.getElementById('drinkIngr').value;
     const drinkNameDiv = document.getElementById('drinkNameDiv');
@@ -22,14 +20,10 @@ async function findDrink(event) {
                 response.json().then(function (data) {
     
                     for (let i = 0; i < data.drinks.length; i++) {
-                        console.log(data.drinks[i].strDrink);
-    
                         const drinkTitle = document.createElement("p");
                         drinkTitle.textContent = data.drinks[i].strDrink;
                         drinkNameDiv.appendChild(drinkTitle);
                     }
-    
-                    console.log("========================")
                 });
             } else {
                 alert("Error: " + response.statusText);
@@ -49,15 +43,10 @@ async function findDrink(event) {
                 response.json().then(function (data) {
     
                     for (let i = 0; i < data.drinks.length; i++) {
-                        console.log(data.drinks[i].strDrink);
-    
                         const drinkTitle = document.createElement("p");
-                        drinkTitle.classList.add("horizontal-center");
                         drinkTitle.textContent = data.drinks[i].strDrink;
                         drinkNameDiv.appendChild(drinkTitle);
                     }
-    
-                    console.log("========================")
                 });
             } else {
                 alert("Error: " + response.statusText);
@@ -66,14 +55,10 @@ async function findDrink(event) {
     }
 
     else if (drinkName && drinkIngr) {
-        // Display message that says only one field at a time
-
         alert("Search by EITHER drink name OR alchohol");
     }
 
     else {
-        // Display message that says to put in one of the fields
-
         alert("Enter in a Drink's Name or an Alcohol");
     }
 
