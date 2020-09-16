1cc36d82b4b0568afda39e77fa13f50e99d5965f
Jacob Daughtry <akutozo@gmail.com>
2020-09-15 19:06:10 -0500

    Added Mingle logic for GET all and POST

---
+++ controllers/api/mingle-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Post, User, Discuss, Mingle } = require("../../models");
+const sequelize = require('../../config/connection');
+
+// Get all users
+router.get('/', (req, res) => {
+    Mingle.findAll()
+        .then(dbMingleData => res.json(dbMingleData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    if (req.session) {
+        Mingle.create({
+            title: req.body.title,
+            description: req.body.description
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+module.exports = router;
