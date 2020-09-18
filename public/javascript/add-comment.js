19acc5bbc5f3ce68874aabfd7814341138108ced
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-09-18 13:49:27 -0500

    pushing the comment progress so far

--- public/javascript/add-comment.js
+++ public/javascript/add-comment.js
@@ -1,18 +1,18 @@
 async function commentFormHandler(event) {
     event.preventDefault();
   
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const opinion = document.querySelector('textarea[name="comment-body"]').value.trim();
   
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
   
-    if (comment_text) {
+    if (opinion) {
         const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
                 post_id,
-                comment_text
+                opinion
             }),
             headers: {
                 'Content-Type': 'application/json'
