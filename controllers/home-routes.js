0dc94705411a740428b263af1d3456000ce564ea
Jacob Daughtry <akutozo@gmail.com>
2020-09-18 20:13:38 -0500

    Added  Navs  for  Recipes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,7 +34,7 @@ router.get('/', (req, res) => {
             const posts = dbPostData.map(post => post.get({ plain: true }))
             res.render('homepage', { 
                 posts,
-                loggedIn: true
+                loggedIn: req.session.loggedIn
             });
         })
         .catch(err => {
