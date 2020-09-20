1282d65ba82a0a718b3e2a3fccc371b2ae27505a
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-09-20 12:02:02 -0500

    searched drinks now display drink name, img, and instructions

--- public/javascript/drinkInfo.js
+++ public/javascript/drinkInfo.js
@@ -1,7 +1,34 @@
-const choice = document.getElementsByClassName("drink");
+const searchResultChoiceName = document.getElementById("searchResultChoiceName");
+const searchResultChoiceImage = document.getElementById("searchResultChoiceImage");
+const searchResultInst = document.getElementById("searchResultInst");
 
-async function drinkInfo() {
-    console.log(this);
-};
+document.onclick = function(event) {
+    const choice = event.target;
+    if (choice.className === "drink") {
+        const drinkName = choice.getAttribute("data");    
+    
+        const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName;
 
-choice.addEventListener("click", drinkInfo);
+        fetch(apiUrl).then(function (response) {
+            if (response.ok) {
+                // send the parsed JSON data
+                response.json().then(function (data) {
+                    const drinkTitle = data.drinks[0].strDrink;
+                    const drinkImg = data.drinks[0].strDrinkThumb + "/preview";
+                    const drinkInst = data.drinks[0].strInstructions;
+
+                    searchResultChoiceName.textContent = drinkTitle;
+                    searchResultChoiceImage.setAttribute("src", drinkImg);
+                    searchResultInst.textContent = drinkInst;
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            };
+        });
+
+    }
+    
+    
+
+    
+}
